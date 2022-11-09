0af91c3b9c0bd98b496964dc7f1de05cee5f5614
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-11-09 16:20:00 +0300

    refactor: return id from loader

--- src/pages/notes/[id]/edit/page.tsx
+++ src/pages/notes/[id]/edit/page.tsx
@@ -1,22 +1,24 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useCallback } from "react";
 import { Await, useLoaderData, useNavigate } from "react-router-dom";
 import { updateNode } from "../../../../api";
 import { useAuthContext } from "../../../../components/authContextProvider/authContextProvider";
 import FormBuilder from "../../../../components/formBuilder/FormBuilder";
 import BackButton from "../../../../components/navigationElements/BackButton";
 import EditNoteSkeleton from "../../../../components/skeletons/EditNoteSkeleton";
-import { Note } from "../../../../types/note";
 import { NotFoundRedirect } from "../../../404";
 import { loader as noteLoader } from "../page";
 
 const EditNotePage = () => {
   const navigate = useNavigate();
   const { user } = useAuthContext();
-  const { getNoteInfo } = useLoaderData() as ReturnType<typeof noteLoader>;
-  const submitHandler = (note: Note) => (payload: Record<string, string>) =>
-    updateNode({ ...note, ...payload }).then((note) =>
-      navigate(`/notes/${note.id}`)
-    );
+  const { getNoteInfo, id } = useLoaderData() as ReturnType<typeof noteLoader>;
+  const submitHandler = useCallback(
+    (payload: Record<string, string>) =>
+      updateNode({ id, ...payload }).then((note) =>
+        navigate(`/notes/${note.id}`)
+      ),
+    [id, navigate]
+  );
   return (
     <div className="flex flex-col gap-3">
       <BackButton />
@@ -29,7 +31,7 @@ const EditNotePage = () => {
             <>
               <h3 className="font-bold text-center text-xl">Edit Note</h3>
               <FormBuilder
-                onSumbit={submitHandler(note)}
+                onSumbit={submitHandler}
                 controls={[
                   {
                     name: "title",
--- src/pages/notes/[id]/page.tsx
+++ src/pages/notes/[id]/page.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useCallback } from "react";
 import {
   Await,
   LoaderFunctionArgs,
@@ -15,7 +15,14 @@ import { NotFoundRedirect } from "../../404";
 const NotePage = () => {
   const navigate = useNavigate();
   const { user } = useAuthContext();
-  const { getNoteInfo } = useLoaderData() as ReturnType<typeof loader>;
+  const { getNoteInfo, id } = useLoaderData() as ReturnType<typeof loader>;
+  const deleteHandler = useCallback(() => {
+    deleteNote(id);
+    navigate(`/notes`);
+  }, [id, navigate]);
+  const editHandler = useCallback(() => {
+    navigate(`/notes/${id}/edit`);
+  }, [id, navigate]);
   return (
     <div className="flex flex-col gap-3">
       <BackButton />
@@ -28,13 +35,7 @@ const NotePage = () => {
             <>
               <div className="flex gap-1 items-center">
                 <h3 className="font-bold text-center text-xl">{note.title} </h3>
-                <NoteControls
-                  onEdit={() => navigate(`/notes/${note.id}/edit`)}
-                  onDelete={() => {
-                    deleteNote(note.id);
-                    navigate(`/notes`);
-                  }}
-                />
+                <NoteControls onEdit={editHandler} onDelete={deleteHandler} />
               </div>
               <p>{note.body} </p>
             </>
@@ -50,5 +51,6 @@ export const loader = ({ params }: LoaderFunctionArgs) => {
   const id = params.id ? (isNaN(+params.id) ? 0 : +params.id) : 0;
   return {
     getNoteInfo: (userId: number) => getUserNoteById(userId, id),
+    id,
   };
 };
